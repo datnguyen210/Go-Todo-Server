c8a5987596f4908db3215d575c1d95e3fc1d6d79
Dat Nguyen <datnguyenftu210@gmail.com>
2024-03-26 00:34:02 +0700

    feat: add endpoint creating a new todo

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
 var todos = MockTodos()
@@ -12,10 +13,38 @@ var todos = MockTodos()
 func main() {
 	router := gin.Default()
 	router.GET("/todos", getTodos)
+	router.POST("/todos", createTodo)
 	router.Run("localhost:8080")
 	fmt.Println("Hello users of the todo list app")
 }
 
 func getTodos(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, todos)
+}
+
+func createTodo(c *gin.Context) {
+	var newTodo Todo
+
+	// Bind JSON payload to newTodo struct
+	if err := c.BindJSON(&newTodo); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Assign ID for the new to-do
+	newTodo.ID = generateID()
+
+	// Append the new to-do to the todos list
+	todos = append(todos, newTodo)
+
+	// Respond with the newly created to-do
+	c.IndentedJSON(http.StatusCreated, newTodo)
+}
+
+func generateID() string {
+	// Generate a UUID version 4 (random UUID)
+	id := uuid.New()
+
+	// Convert UUID to string
+	return id.String()
 }
