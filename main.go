b15bc57ecc8d3195758ca82e96036d1a90090c0e
Dat Nguyen <datnguyenftu210@gmail.com>
2024-05-09 07:40:36 +0700

    feat: connect server to a local database

--- main.go
+++ main.go
@@ -1,19 +1,12 @@
 package main
 
 import (
-	"fmt"
-
 	"datnguyen/todo/database"
-
-	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	router := gin.Default()
-	router.GET("/todos", database.GetTodos)
-	router.GET("todos/:id", database.GetTodoById)
-	router.POST("/todos", database.CreateTodo)
-	router.PUT("/todos/:id", database.UpdateTodo)
-	router.Run("localhost:8080")
-	fmt.Println("Hello users of the todo list app")
+	// Initialize the database connection
+	database.DatabaseInit()
+
+	println("Initialized!")
 }
